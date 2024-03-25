26da59a93c6e223db7d105636aa396a4a0e6ebb7
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-03-25 13:43:12 -0400

    Add starter test html, scripts, and styles

---
+++ assets/scripts/test_scripts.js
@@ -0,0 +1,11 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 19 Mar 2024, 7:20:40 PM
+ *  Last update: 19 Mar 2024, 8:19:16 PM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+//document.getElementById("teamBuilder").addEventListener("click", () => {
+document.querySelector(".logo").addEventListener("click", () => {
+    // alert("test!");
+    document.querySelector(".page-deco-1").classList.toggle("flip-off-page");
+});
